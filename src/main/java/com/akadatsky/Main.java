f7f314ef218951eccb8e86b4de69a9e7120a61b7
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-03-21 12:56:51 +0200

    initial commit

---
+++ src/main/java/com/akadatsky/Main.java
@@ -0,0 +1,14 @@
+package com.akadatsky;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("TEST");
+
+        Gson gson = new Gson();
+
+    }
+
+}
